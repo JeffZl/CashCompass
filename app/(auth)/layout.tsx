9904c70b1534eff462aaa5751f9e65b9ade6fa96
JeffZl <86556813+JeffZl@users.noreply.github.com>
2025-12-07 11:53:20 +0700

    added authentication using clerk

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,8 @@
+const layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            {children}
+        </div>
+    )
+}
+export default layout
